e40a477096b2f61b5d8489340c1a1efe7f34fab4
Paolo Bacchilega <paobac@src.gnome.org>
2004-10-25 22:42:17 +0000

    2004-10-26  Paolo Bacchilega  <paobac@cvs.gnome.org
    	* src/gthumb-enum-types.c:
            * src/gthumb-enum-types.h:
            * src/gthumb-slide.c:
            * src/gthumb-slide.h: new files.

---
+++ src/gthumb-enum-types.h
@@ -0,0 +1,24 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef GTHUMB_ENUM__TYPES_H
+#define GTHUMB_ENUM_TYPES_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/* enumerations from "gth-image-list.h" */
+
+GType gth_cursor_movement_get_type (void);
+#define GTH_TYPE_CURSOR_MOVEMENT (gth_cursor_movement_get_type())
+
+GType gth_selection_change_get_type (void);
+#define GTH_TYPE_SELECTION_CHANGE (gth_selection_change_get_type())
+
+G_END_DECLS
+
+#endif /* GTHUMB_ENUM_TYPES_H */
+
+/* Generated data ends here */
+
