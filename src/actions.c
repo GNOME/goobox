c607af9305366bf6a9d4d3fb0d25608e4ca0573a
Helge Kreutzmann <debian@helgefjell.de>
2009-11-23 09:38:18 +0100

    fixed white spaces in the license text

--- src/actions.c
+++ src/actions.c
@@ -140,7 +140,7 @@ activate_action_about (GtkAction *action,
 	const char *translator_credits = _("translator_credits");
         char       *license_text;
         const char *license[] = {
-                N_("CD Player is free software; you can redistribute it and/or modify"
+                N_("CD Player is free software; you can redistribute it and/or modify "
                 "it under the terms of the GNU General Public License as published by "
                 "the Free Software Foundation; either version 2 of the License, or "
                 "(at your option) any later version."),
@@ -150,7 +150,7 @@ activate_action_about (GtkAction *action,
                 "GNU General Public License for more details."),
                 N_("You should have received a copy of the GNU General Public License "
                 "along with CD Player; if not, write to the Free Software Foundation, Inc., "
-                "51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA")
+                "51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA")
         };
 
         license_text = g_strconcat (license[0], "\n\n", license[1], "\n\n",
