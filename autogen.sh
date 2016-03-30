f24ca3db113c1478f4d1244f60366f03d9d963b5
Paolo Bacchilega <paobac@src.gnome.org>
2016-03-30 11:33:10 +0200

    autogen: removed obsolete definitions

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="Goobox"
-
 (test -f $srcdir/configure.ac && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
@@ -19,4 +17,4 @@ which gnome-autogen.sh || {
 
 REQUIRED_AUTOMAKE_VERSION=1.7.2
 
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+. gnome-autogen.sh
